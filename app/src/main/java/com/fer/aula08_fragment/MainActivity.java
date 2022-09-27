bd5d738685d97e133d1e26a8ddede38e50fd7fe9
ferdinandiz <80607754+ferdinandiz@users.noreply.github.com>
2022-09-27 20:00:28 -0300

    aula desastrosa

--- app/src/main/java/com/fer/aula08_fragment/MainActivity.java
+++ app/src/main/java/com/fer/aula08_fragment/MainActivity.java
@@ -1,10 +1,13 @@
 package com.fer.aula08_fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
